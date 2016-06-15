a1f50393ccf0d320ebfd999eb72859d2a4646a13
Boris Litvinsky <borisl@wix.com>
2016-06-15 15:48:03 +0300

    #ref syntax for getting value + (click) syntax

--- app/todo-input.ts
+++ app/todo-input.ts
@@ -3,10 +3,13 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'todo-input',
   template: `
-    <input type="text">
-    <button type="button">Add</button>
+    <input type="text" #todoInput/>
+    <button type="button" (click)="onAddClicked(todoInput.value)">Add</button>
+
   `
 })
 export class TodoInput {
-
+  onAddClicked(val) {
+    console.log(val);
+  }
 }
