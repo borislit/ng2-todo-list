31d9be85da526fbc4c7deb878a0cc19004588b87
Boris Litvinsky <borisl@wix.com>
2016-06-15 15:58:14 +0300

    move to [value]/(input)

--- app/todo-input.ts
+++ app/todo-input.ts
@@ -3,14 +3,15 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'todo-input',
   template: `
-    <input type="text" #todoInput/>
-    <button type="button" (contextmenu)="onAddClicked(todoInput.value, $event)">Add</button>
-
+    <input type="text" #todoInput [value]="defaultValue" (input)="defaultValue = todoInput.value"/>
+    <button type="button" (click)="onAddClicked(todoInput.value)">Add</button>
+    {{defaultValue}}
   `
 })
 export class TodoInput {
-  onAddClicked(val, ev) {
-    ev.preventDefault();
-    console.log(val);
+  defaultValue = 'Enter Value Here';
+
+  onAddClicked() {
+    console.log(this.defaultValue);
   }
 }
